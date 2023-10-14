f4c54efdac917c305837f529ce810d1bbd46776f
srinivas.kool <31861607+srinivaskool@users.noreply.github.com>
2023-10-14 16:28:32 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+const OpenAI = require('openai');
 const express = require('express');
 const axios = require('axios');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const https = require("https");
+require("dotenv").config();
+
 
 const app = express();
 const port = 3004;
@@ -25,39 +29,71 @@ app.get('/hi', (req, res) => {
 
 // Define a route for handling the request
 app.post('/openai-chat', async (req, res) => {
-    try {
+    // try {
+    //     const fetch = await import('node-fetch');
+    //     const response = await fetch('https://api.openai.com/v1/chat/completions', {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //             'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+    //         },
+    //         body: JSON.stringify({
+    //             model: 'gpt-3.5-turbo',
+    //             max_tokens: 1000,
+    //             temperature: 0.7,
+    //             n: 1,
+    //             messages: [
+    //                 {
+    //                     "role": "assistant",
+    //                     "content": "Hello Siri Chandana Pulakanti! Shall we start the first round of interview?"
+    //                 },
+    //                 {
+    //                     "role": "user",
+    //                     "content": "go"
+    //                 }
+    //             ],
+    //             stream: true,
+    //         }),
+    //     });
 
-        const response = await axios.post(
-            'https://api.openai.com/v1/chat/completions',
+    //     const responseData = await response.json();
+    //     res.json(responseData);
+    // } catch (error) {
+    //     console.error(error);
+    //     res.status(500).json({ error: error });
+    // }
+
+    const options = {
+        hostname: "api.openai.com",
+        path: "/v1/chat/completions",
+        port: 443,
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+        },
+    };
+    const payload = {
+        model: "gpt-3.5-turbo",
+        messages: [
             {
-                model: 'gpt-3.5-turbo',
-                max_tokens: 1000,
-                temperature: 0.7,
-                n: 1,
-                "messages": [
-                    {
-                        "role": "assistant",
-                        "content": "Hello Siri Chandana Pulakanti! Shall we start the first round of interview?"
-                    },
-                    {
-                        "role": "user",
-                        "content": "go"
-                    }
-                ],
-                stream: true,
+                role: "user",
+                content: "say hello world",
             },
-            {
-                headers: {
-                    'Authorization': 'Bearer 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'
-                }
-            }
-        );
-
-        res.json(response.data);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: error });
-    }
+        ],
+        temperature: 0.7,
+        stream: true,
+    };
+    const request = https.request(options, (res) => {
+        res.on("data", (chunk) => {
+            console.log(chunk.toString());
+        });
+        res.on("end", () => {
+            console.log("done");
+        });
+    });
+    request.write(JSON.stringify(payload));
+    request.end();
 });
 
 app.listen(port, () => {
