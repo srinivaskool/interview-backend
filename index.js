24d025669e4b2590d10713765a9b4ac65c074cf4
srinivas.kool <31861607+srinivaskool@users.noreply.github.com>
2023-10-14 15:04:16 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,62 +1,14 @@
 const express = require('express');
+const axios = require('axios');
 const bodyParser = require('body-parser');
-var axios = require("axios");
-const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 3004;
+const port = 3004;
 
-// Middleware to parse JSON body
 app.use(bodyParser.json());
 
-// Enable CORS for all routes
-app.use(cors()); // Use cors middleware to enable CORS
-
-// Define a POST route
-app.post('/linkedin-login', async (req, res) => {
-    try {
-
-        const requestData = req.body;
-        const authorizationCode = requestData.code;
-        console.log('authorizationCode', authorizationCode);
-
-        const clientId = '86agn2chxp9rtd';
-        const clientSecret = 'iUoRpiFNFrmDtqGC';
-        const redirectUri = 'http://localhost:3000/';
-
-        let accessToken;
-
-        await axios
-            .post('https://www.linkedin.com/oauth/v2/accessToken', null, {
-                params: {
-                    grant_type: 'authorization_code',
-                    code: authorizationCode,
-                    redirect_uri: redirectUri,
-                    client_id: clientId,
-                    client_secret: clientSecret,
-                },
-            })
-            .then((response) => {
-                accessToken = response.data.access_token;
-                console.log('accessToken', accessToken);
-            })
-            .catch((error) => {
-                console.error('Error exchanging code for access token:', error);
-            });
-
-        const response = await axios.get('https://api.linkedin.com/v2/userinfo', {
-            headers: {
-                Authorization: `Bearer ${accessToken}`,
-            },
-        });
-        console.log('User Profile:', response.data);
-        res.status(200).json({ message: 'Data received successfully', data: response.data });
-    } catch (error) {
-        // Handle errors
-        console.error(error);
-        res.status(500).json({ error: error });
-    }
-});
+// Use bodyParser for parsing JSON
+app.use(express.json());
 
 app.get('/hi', (req, res) => {
     try {
@@ -68,7 +20,43 @@ app.get('/hi', (req, res) => {
     }
 });
 
-// Start the server
+// Define a route for handling the request
+app.post('/openai-chat', async (req, res) => {
+    try {
+
+        const response = await axios.post(
+            'https://api.openai.com/v1/chat/completions',
+            {
+                model: 'gpt-3.5-turbo',
+                max_tokens: 1000,
+                temperature: 0.7,
+                n: 1,
+                "messages": [
+                    {
+                        "role": "assistant",
+                        "content": "Hello Siri Chandana Pulakanti! Shall we start the first round of interview?"
+                    },
+                    {
+                        "role": "user",
+                        "content": "go"
+                    }
+                ],
+                stream: true,
+            },
+            {
+                headers: {
+                    'Authorization': 'Bearer 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'
+                }
+            }
+        );
+
+        res.json(response.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'An error occurred' });
+    }
+});
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
