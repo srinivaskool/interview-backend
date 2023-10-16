a169a5e3e9caf1bfcd479a387353b8d22887f1a9
srinivas.kool <31861607+srinivaskool@users.noreply.github.com>
2023-10-16 08:55:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import OpenAI from 'openai';
 import express from 'express';
-import axios from 'axios';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import https from 'https';
 import fetch from 'node-fetch';
 
 const app = express();
@@ -25,74 +23,6 @@ app.get('/hi', (req, res) => {
     }
 });
 
-// Define a route for handling the request
-app.post('/openai-chat', async (req, res) => {
-    // try {
-    //     const fetch = await import('node-fetch');
-    //     const response = await fetch('https://api.openai.com/v1/chat/completions', {
-    //         method: 'POST',
-    //         headers: {
-    //             'Content-Type': 'application/json',
-    //             'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
-    //         },
-    //         body: JSON.stringify({
-    //             model: 'gpt-3.5-turbo',
-    //             max_tokens: 1000,
-    //             temperature: 0.7,
-    //             n: 1,
-    //             messages: [
-    //                 {
-    //                     "role": "assistant",
-    //                     "content": "Hello Siri Chandana Pulakanti! Shall we start the first round of interview?"
-    //                 },
-    //                 {
-    //                     "role": "user",
-    //                     "content": "go"
-    //                 }
-    //             ],
-    //             stream: true,
-    //         }),
-    //     });
-
-    //     const responseData = await response.json();
-    //     res.json(responseData);
-    // } catch (error) {
-    //     console.error(error);
-    //     res.status(500).json({ error: error });
-    // }
-
-    const options = {
-        hostname: "api.openai.com",
-        path: "/v1/chat/completions",
-        port: 443,
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-        },
-    };
-    const payload = {
-        model: "gpt-3.5-turbo",
-        messages: [
-            {
-                role: "user",
-                content: "say hello world",
-            },
-        ],
-        temperature: 0.7,
-        stream: true,
-    };
-    const request = https.request(options, (res) => {
-        res.on("data", (chunk) => {
-            console.log(chunk.toString());
-        });
-        res.on("end", () => {
-            console.log("done");
-        });
-    });
-    request.write(JSON.stringify(payload));
-    request.end();
-});
 
 app.post('/forward', async (req, res) => {
   try {
